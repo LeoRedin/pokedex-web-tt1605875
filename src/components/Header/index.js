e3cc6f4ea4215868126736a68c4f1de9d44f92cd
Leonardo Redin <leonardoredin@outlook.com>
2021-01-27 21:38:58 -0300

    initial header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+/* MENU */
+/* 
+  4 LINKS
+  Home, Pokemons, Gerações, Sobre
+*/
+function Header() {
+  return (
+    <div>
+      <div>LOGO</div>
+      <div>MENU</div>
+    </div>
+  )
+}
+
+export { Header }
