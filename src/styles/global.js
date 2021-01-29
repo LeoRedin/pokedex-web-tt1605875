3a36d2e5ff5f86ef825bedebeae749c5a96b29c9
Leonardo Redin <leonardoredin@outlook.com>
2021-01-28 21:10:17 -0300

    adicionando fonte

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
+import SFRegular from '../assets/fonts/sf-pro-display-regular.ttf'
+
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Sf';
+    src: local('Sf'), local('FontName'),
+    url(${SFRegular}) format('woff');
+    font-weight: 300;
+    font-style: normal;
+  }
+
   body {
+    font-family: 'Sf';
     background-color: #ff9227;
   }
 `
