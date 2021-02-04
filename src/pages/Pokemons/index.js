e157f441e99d5de960ac58a0ce6a654988ef8dc3
Leonardo Redin <leonardoredin@outlook.com>
2021-02-04 19:56:01 -0300

    inicio do fetch dos pokemons

--- src/pages/Pokemons/index.js
+++ src/pages/Pokemons/index.js
@@ -1,4 +1,17 @@
+import * as React from 'react'
+
+import { api } from '../../services'
+
 function Pokemons() {
+  React.useEffect(() => {
+    async function fetchPokemons() {
+      const fetchData = await api.get('/pokemon')
+      console.log(fetchData.data.results)
+    }
+
+    fetchPokemons()
+  }, [])
+
   return <div>POKEMONS</div>
 }
 
